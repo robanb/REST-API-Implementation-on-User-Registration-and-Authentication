b00be04b278fd5e22a443442b2c1c099275762ba
Roba Nath Basnet <robanbth@gmail.com>
2022-11-01 23:29:10 +0600

    User Model Created

---
+++ api/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	name: { type: String, required: true },
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		match:
+			/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+	},
+	birthdate: { type: Date, required: true },
+	Contac_Number: { type: Number, required: true },
+	profession: { type: String },
+	Married: { type: Boolean, required: true },
+	password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);

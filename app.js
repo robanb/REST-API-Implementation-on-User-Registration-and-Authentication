6eb5639aa1f247091970d933a3ef09908a16cc17
Roba Nath Basnet <robanbth@gmail.com>
2022-11-02 00:26:52 +0600

    Added BodyParser, and handled CORS Error

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// middleware to log incoming request
+app.use(morgan("dev"));
+
+// parse incoming request with urlencoded payloads
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// extract json data
+app.use(bodyParser.json());
+
+// Adjust the Header payload to prevent CORS(Cross-Origin Resourse Sharing) error
+app.use((req, res, next) => {
+	res.header("Allow-Control-Allow_Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+
+	// Checking for allowed http methods
+	if (req.method === "OPTIONS") {
+		res.header(
+			"Access-Control-Allowed-Methods",
+			"PUT, POST, PATCH, DELETE, GET"
+		);
+		return res.status(200).json({});
+	}
+	next();
+});
+
+module.exports = app;

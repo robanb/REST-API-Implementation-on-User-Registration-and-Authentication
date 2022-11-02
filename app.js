93ed22ec32f09a4b89c2585551da489d439c8706
Roba Nath Basnet <robanbth@gmail.com>
2022-11-02 13:26:44 +0600

    Configured Error Handling

--- app.js
+++ app.js
@@ -38,5 +38,21 @@ app.use((req, res, next) => {
 // Setting up middleware
 app.use("/user", UserRoutes);
 
+// Error handling using predefined error object
+app.use((req, res, next) => {
+	const error = new Error("Not found");
+	error.status = 404;
+	next(error);
+});
+
+// Handling all errors
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message,
+		},
+	});
+});
 
 module.exports = app;

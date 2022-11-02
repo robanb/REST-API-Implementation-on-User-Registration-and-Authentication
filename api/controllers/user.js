f3d46985850d3f72ddeea320c77907535faec47e
Roba Nath Basnet <robanbth@gmail.com>
2022-11-02 12:10:46 +0600

    User controllers added for Registeration, authentication and Deletion

---
+++ api/controllers/user.js
@@ -0,0 +1,112 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+// User registeration controller
+exports.user_signup = (req, res, next) => {
+	User.findOne({ Email: req.body.Email })
+		.exec()
+		.then((user) => {
+			if (user) {
+				return res.status(409).json({
+					// status(404): indicating a conflict
+					message: "User Already Exist!",
+				});
+			} else {
+				bcrypt.hash(req.body.password, 10, (err, hash) => {
+					if (err) {
+						return res.status(500).json({
+							error: err,
+						});
+					} else {
+						const user = new User({
+							_id: new mongoose.Types.ObjectId(),
+							Name: req.body.Name,
+							Email: req.body.Email,
+							BirthDate: req.body.BirthDate,
+							ContacNumber: req.body.ContacNumber,
+							Profession: req.body.Profession,
+							Married: req.body.Married,
+							Image: req.file.path,
+							Password: hash,
+						});
+						user
+							.save()
+							.then((result) => {
+								res.status(201).json({
+									message: "User Registeration Successful!",
+								});
+							})
+							.catch((err) => {
+								res.status(500).json({
+									error: err,
+								});
+							});
+					}
+				});
+			}
+		});
+};
+
+// User Authentication Controller
+exports.user_login = (req, res, next) => {
+	User.findOne({ Email: req.body.Email })
+		.exec()
+		.then((user) => {
+			if (!user) {
+				// 401: Unauthorized
+				return res.status(401).json({
+					message: "User doesn't exist!",
+				});
+			}
+			bcrypt.compare(req.body.Password, user.Password, (err, result) => {
+				if (err) {
+					return res.status(401).json({
+						message: "Authentication failed!",
+					});
+				}
+				if (result) {
+					const token = jwt.sign(
+						{
+							email: user.Email,
+							userID: user._id,
+						},
+						process.env.JWY_KEY,
+						{
+							expiresIn: "1h",
+						}
+					);
+					return res.status(200).json({
+						message: "Authentication Successful",
+						token: token,
+					});
+				}
+				return res.status(401).json({
+					message: "Authentication failed!",
+				});
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+			});
+		});
+};
+
+// User Deletion: To allow only the loggedin user to delete his/her account
+
+exports.user_delete = (req, res, next) => {
+	User.deleteOne({ _id: req.params.userID })
+		.exec()
+		.then((result) => {
+			res.status(200).json({
+				message: "User Deletion Successful",
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+			});
+		});
+};
--- api/models/user.js
+++ api/models/user.js
@@ -2,20 +2,20 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
-	name: { type: String, required: true },
-	email: {
+	Name: { type: String, required: true },
+	Email: {
 		type: String,
 		required: true,
 		unique: true,
 		match:
 			/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
 	},
-	birthdate: { type: Date, required: true },
-	Contac_Number: { type: Number, required: true },
-	profession: { type: String },
+	BirthDate: { type: Date, required: true },
+	ContacNumber: { type: Number, required: true },
+	Profession: { type: String },
 	Married: { type: Boolean, required: true },
 	Image: { type: String, required: true },
-	password: { type: String, required: true },
+	Password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);

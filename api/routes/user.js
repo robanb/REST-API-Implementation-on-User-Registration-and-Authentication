67443e26de518c6f22de8f311b0925b6178c8602
Roba Nath Basnet <robanbth@gmail.com>
2022-11-02 12:49:08 +0600

    Configured File upload, fileFilter, fileSize for ProfileImage

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 const UserController = require("../controllers/user");
 
+// ProfileImage Storage
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./uploads");
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + file.originalname);
+	},
+});
+
+// Filtering the ProfileImage file
+const fileFilter = (req, res, cb) => {
+	if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+		cb(null, true);
+	} else {
+		cb(null, flase);
+	}
+};
+
+// Process file upload via multer and allocate file size limit (to 5 MB)
+const upload = multer({
+	storage: storage,
+	limits: {
+		fileSize: 1024 * 1024 * 5,
+	},
+	fileFilter: fileFilter,
+});
+
 // User Registeration Route
-router.post("/signup", UserController.user_signup);
+router.post(
+	"/signup",
+	upload.single("ProfileImage"),
+	UserController.user_signup
+);
 
 // User Authentication Route
 router.post("/login", UserController.user_login);

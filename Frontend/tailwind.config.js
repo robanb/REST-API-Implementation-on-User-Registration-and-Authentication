49ce61e99480c7b6c1a10ebb2cf0883c9a576865
Roba Nath Basnet <robanbth@gmail.com>
2022-11-09 16:41:22 +0600

    Setup

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			width: {
+				"40-r": "40rem",
+				"35-r": "35rem",
+				"30-r": "30rem",
+			},
+			height: {
+				"40-r": "40rem",
+				"35-r": "35rem",
+				"30-r": "30rem",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+			boxShadow: {
+				"5xl": "20px 20px 50px rgba(0, 0, 0, 0.5)",
+			},
+		},
+	},
+	plugins: [],
+};
